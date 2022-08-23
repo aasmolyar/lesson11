47a092af06b20a49d15c52a509b4f98779ec0696
Andrey Smolyar <smolyar2022@internet.ru>
2022-08-23 20:46:39 +0500

    Урок11

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+const btn = document.getElementById('btn');
+
+const square = document.getElementById('square');
+console.log(square);
+//square.style.backgroundColor = 'blue'
+
+const text = document.getElementById('text');
+console.log(text);
 
 
+let color;
+
+const logger = function (event) {
+    color = event.target.value;
+    console.log('в логере ' + color);
+}
 
 
+const changeColor = function (event) {
+    console.log('в чендже ' + color);
+    square.style.backgroundColor = color;
+}
 
+text.addEventListener('input', logger)
+btn.addEventListener('click', changeColor)
