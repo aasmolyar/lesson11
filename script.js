5d559a9694dd8cc26993144ec6e3de1ede27476d
Andrey Smolyar <smolyar2022@internet.ru>
2022-08-24 13:11:22 +0500

    Задание11

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
+
 const btn = document.getElementById('btn');
-
 const square = document.getElementById('square');
-console.log(square);
-//square.style.backgroundColor = 'blue'
-
 const text = document.getElementById('text');
-console.log(text);
-
 
 let color;
-
 const logger = function (event) {
     color = event.target.value;
-    console.log('в логере ' + color);
 }
 
-
 const changeColor = function (event) {
-    console.log('в чендже ' + color);
     square.style.backgroundColor = color;
 }
 
 text.addEventListener('input', logger)
-btn.addEventListener('click', changeColor)
+btn.addEventListener('click', changeColor);
+
+const ebtn = document.getElementById('e_btn');
+
+const changeStyle = function (event) {
+    ebtn.style.display = 'none';
+}
+
+ebtn.addEventListener('click', changeStyle);
+
+
+const range = document.getElementById('range');
+console.log(range);
+const span = document.getElementById('range-span');
+const circle = document.getElementById('circle');
+console.dir(circle);
+
+span.textContent = '50%';
+circle.style.height = `${span.textContent}`;
+circle.style.width = `${span.textContent}`;
+
+let size;
+
+const logger1 = function (event) {
+    span.textContent = event.target.value + '%';
+    size = event.target.value;
+}
+
+const changeSize = function (event) {
+    circle.style.height = size + '%';
+    circle.style.width = size + '%';
+}
+
+range.addEventListener('input', logger1);
+range.addEventListener('input', changeSize);
+
